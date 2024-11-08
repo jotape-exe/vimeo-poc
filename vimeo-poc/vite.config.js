c3cba82a88636c8dc38c4aac959908cd259848bf
jotape-exe <joaoxstone@gmail.com>
2024-11-08 18:51:22 -0300

    build

--- vimeo-poc/vite.config.js
+++ vimeo-poc/vite.config.js
@@ -1,7 +1,8 @@
 
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
